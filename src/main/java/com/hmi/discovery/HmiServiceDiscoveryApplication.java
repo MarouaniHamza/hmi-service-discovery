8e81cc29a2bcaea39400614cc98d52d556734501
MarouaniHamza <hamza.marouenihamza@gmail.com>
2020-03-22 11:58:51 +0100

    Setup the discovery micro-service

---
+++ src/main/java/com/hmi/discovery/HmiServiceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.hmi.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class HmiServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmiServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmi/discovery/HmiServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmi.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmiServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
